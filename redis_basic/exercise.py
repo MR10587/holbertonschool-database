629db0242e13975aed7287f3ecee2d615d5875c5
MR10587 <ramalmirzezade20@gmail.com>
2026-03-21 07:41:33 +0000

    .

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 '''Redis Quickstart'''
-import redis
-import uuid
-import typing
+try:
+    import redis
+    import uuid
+    import typing
 
 
-class Cache:
-    '''Cache class'''
-    def __init__(self):
-        '''Redis instance flushed'''
-        self._redis = redis.Redis()
-        self._redis.flushdb()
+    class Cache:
+        '''Cache class'''
+        def __init__(self):
+            '''Redis instance flushed'''
+            self._redis = redis.Redis()
+            self._redis.flushdb()
 
-    def store(self, data: typing.Union[str, float, int, bytes]) -> str:
-        '''Storing data in random key'''
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        def store(self, data: typing.Union[str, float, int, bytes]) -> str:
+            '''Storing data in random key'''
+            key = str(uuid.uuid4())
+            self._redis.set(key, data)
+            return key
+except ModuleNotFoundError:
+    print("Salam")
