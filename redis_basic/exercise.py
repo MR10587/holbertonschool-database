d236af7afd470461e7af3298bb1f6e892391cb53
MR10587 <ramalmirzezade20@gmail.com>
2026-03-21 07:45:00 +0000

    .# Please enter the commit message for your changes. Lines starting

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -2,21 +2,21 @@
 '''Redis Quickstart'''
 try:
     import redis
-    import uuid
-    import typing
-
-
-    class Cache:
-        '''Cache class'''
-        def __init__(self):
-            '''Redis instance flushed'''
-            self._redis = redis.Redis()
-            self._redis.flushdb()
-
-        def store(self, data: typing.Union[str, float, int, bytes]) -> str:
-            '''Storing data in random key'''
-            key = str(uuid.uuid4())
-            self._redis.set(key, data)
-            return key
-except (AttributeError, ModuleNotFoundError):
+except ModuleNotFoundError:
     print("Salam")
+import uuid
+import typing
+
+
+class Cache:
+    '''Cache class'''
+    def __init__(self):
+        '''Redis instance flushed'''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: typing.Union[str, float, int, bytes]) -> str:
+        '''Storing data in random key'''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
