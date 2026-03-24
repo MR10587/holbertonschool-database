7a8e51caf2a699a923be84d0b6075313320e4ce9
MR10587 <ramalmirzezade20@gmail.com>
2026-03-24 08:04:54 +0000

    .

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -19,7 +19,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable[bytes, any]] = None) -> Any:
+    def get(self, key: str, fn: Optional[Callable[[bytes], Any]] = None) -> Any:
         '''Getting value in desired format'''
         value = self._redis.get(key)
         if fn:
